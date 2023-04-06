0aca1b2d0295335f725c46d79d8b55e2017a0c2c
Antoni Krzyżaniak <73665@ue.poznan.pl>
2023-04-06 15:44:07 +0200

    dodano kwadrat

--- ex_2.py
+++ ex_2.py
@@ -9,3 +9,20 @@ obwod = a + b + c
 pole = int((h * a) / 2)
 
 print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynisi " + str(pole) + ".")
+
+# kwadrat
+
+from cmath import pi, sqrt
+from re import I
+
+
+a = 10 # dlugosc bokow
+
+obwod = a*4
+pole = a*a
+
+# obwod kwadratu wynosi 40, a pole 100.
+
+print("Obwod kwadratu wynosi " + str(obwod) + ", a pole wynosi " + str(pole) + ".")
+
+print(f'Obwod kwadratu wynosi {obwod}, a pole wynosi {pole}.')
